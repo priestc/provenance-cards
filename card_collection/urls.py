6e09206d7e335135f2f13c546f5119b09a3b3cf0
Chris Priest <nbvfour@gmail.com>
2021-03-22 14:06:26 -0600

    added collection pages

--- card_collection/urls.py
+++ card_collection/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import (
-    add_to_collection, collection_overview
+    add_to_collection, collection_overview, set_overview, subset_overview
 )
 
 urlpatterns = [
     path('enter_subset/<int:subset_id>', add_to_collection),
-    path('overview/<str:username>', collection_overview)
+    path('overview/<str:username>', collection_overview),
+    path('set/<int:set_id>/<str:username>', set_overview),
+    path('subset/<int:subset_id>/<str:username>', subset_overview)
 ]
--- card_collection/views.py
+++ card_collection/views.py
@@ -3,11 +3,26 @@ import json
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 
-from card_set.models import Card, Subset
+from card_set.models import Card, Subset, Set
 from .models import CollectionCard
 
 def collection_overview(request, username):
+    collection = CollectionCard.objects.filter(owner__username=username)
+    card_count = collection.count()
+    sets = Set.objects.filter(subset__card__collectioncard__owner__username=username).distinct()
     return render(request, "collection_overview.html", locals())
+
+def set_overview(request, set_id, username):
+    set = Set.objects.get(id=set_id)
+    subsets = Subset.objects.filter(card__collectioncard__owner__username=username, card__subset__set=set).distinct()
+    boxes_of_scarcity = CollectionCard.boxes_of_scarcity_for_user(username, set)
+    return render(request, "set_collection.html", locals())
+
+def subset_overview(request, subset_id, username):
+    subset = Subset.objects.get(id=subset_id)
+    haves = CollectionCard.objects.filter(owner__username=username, card__subset=subset)
+    needs = list(Card.objects.filter(subset=subset).exclude(collectioncard__in=haves))
+    return render(request, "subset_collection.html", locals())
 
 @csrf_exempt
 def add_to_collection(request, subset_id):
